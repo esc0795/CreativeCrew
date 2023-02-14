d5ea227effa5f6ea09b51fbc3f6f0997f5e37457
esibaja0795 <sibaja0795@gmail.com>
2023-02-13 23:54:33 -0600

    Installing the package locally

---
+++ RatonesColoraos/RatonesColoraos/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "control/control.php";
+
+$objControl = new control();// instanciacion( instancia=objControl, new crea objetos, control() es el contructor de clase control)
+
+//$objControl->saludar();
+//phpinfo();
+
+
+
+?>
