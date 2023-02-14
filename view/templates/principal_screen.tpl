084541f0a4751ec3abf6b7800b36788898b34a10
esibaja0795 <sibaja0795@gmail.com>
2023-02-14 15:37:52 -0600

    Build main page

--- view/templates/principal_screen.tpl
+++ view/templates/principal_screen.tpl
@@ -4,7 +4,7 @@
 <style>
 
 body{
-  background-color: #015E7C;
+  background-color: black;
 }
 
 ul {
@@ -12,7 +12,8 @@ ul {
   margin: 0;
   padding: 0;
   overflow: hidden;
-  background-color: #333;
+  background-color: #3D3D3E;
+  opacity: 95%;
 }
 
 li {
@@ -24,32 +25,86 @@ li a {
   color: white;
   font-family: "Century Gothic";
   text-align: center;
-  padding: 16px 199px;
+  padding: 16px 99.8px;
   text-decoration: none;
 }
 
 li a:hover {
   background-color: #A52121;
+  color: black;
+  opacity: 80%;
 }
 
-.header{
-  /*font-family: "Century Gothic";
-  color: #D02828;
-  background-color: #163C48;*/
-  padding: 0px 0px;
+.fotoPrin {
+  opacity: 98%;
+  height: 250px;
+  margin-top: 35px;
+  border-radius: 50px;
+}
+
+.header {
+  width:30em;
+  height:17em;
+  padding-left: 425px;
+  margin-bottom: 15px;
+}
+
+.f1 {
+  font-family: "Century Gothic";
+  color: white;
+  padding-top: 300px;
+  padding-left: 575px;
+}
+
+#tickets{
+  text-decoration: none;
+  font-family: "Century Gothic";
+  color: white;
+  border-style: solid;
+}
+
+.btnTickets {
+  background-color: #232929;
+  height: 40px;
+  width: 75px;
+  border-radius: 10px;
+  color: white;
+}
+
+.btnTickets:hover {
+  background-color: #A52121;
+  color: black;
 }
 
 </style>
 </head>
 <body>
   <div class="header">
-    <img src="https://i.pinimg.com/474x/d1/29/b6/d129b66cd9fb6a653dac16437ffa0eaf.jpg" alt="">
+    <img class="" src="graphics/RCH.png" alt="">
   </div>
-
-  <ul>
-    <li><a class="active" href="#rc">RC!</a></li>
-    <li><a href="#chivos">Chivos</a></li>
-    <li><a href="#contacto">Contacto</a></li>
+  <ul id="menu">
+    <li><a href="">Soundcloud</a></li>
+    <li><a href="">Twitter</a></li>
+    <li><a href="">Instagram</a></li>
+    <li><a href="">YouTube</a></li>
+    <li><a href="">TikTok</a></li>
   </ul>
+  <div class="fotoPrin">
+    <center><img src="graphics/envivo.jpg" alt=""></center>
+  </div>
+  <br>
+  <div class="f1">
+      <h3>Jazz Café Escazú</h3>
+      <h5>Marzo 3, 2023 - 8pm</h5>
+      <button class="btnTickets" type="button" name="button">Tickets</button>
+
+      <h3>El Observatorio</h3>
+      <h5>Marzo 15, 2023 - 8pm</h5>
+      <button  class="btnTickets" type="button" name="button">Tickets</button>
+
+      <h3>Parque Viva</h3>
+      <h5>Marzo 10, 2023 - 8pm</h5>
+      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  </div>
 </body>
 </html>
--- view/templates_c/201e082e24da46c3648012625ccb9f2cda62d8b5_0.file.principal_screen.tpl.php
+++ view/templates_c/201e082e24da46c3648012625ccb9f2cda62d8b5_0.file.principal_screen.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-02-14 18:43:58
+/* Smarty version 4.3.0, created on 2023-02-14 22:36:53
   from 'C:\xampp\htdocs\RatonesColoraos\view\templates\principal_screen.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_63ebc85eb10767_82618763',
+  'unifunc' => 'content_63ebfef5052e87_87886706',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '201e082e24da46c3648012625ccb9f2cda62d8b5' => 
     array (
       0 => 'C:\\xampp\\htdocs\\RatonesColoraos\\view\\templates\\principal_screen.tpl',
-      1 => 1676396636,
+      1 => 1676410611,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63ebc85eb10767_82618763 (Smarty_Internal_Template $_smarty_tpl) {
+function content_63ebfef5052e87_87886706 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 <head>
 <style>
 
 body{
-  background-color: #015E7C;
+  background-color: black;
 }
 
 ul {
@@ -35,7 +35,8 @@ ul {
   margin: 0;
   padding: 0;
   overflow: hidden;
-  background-color: #333;
+  background-color: #3D3D3E;
+  opacity: 95%;
 }
 
 li {
@@ -47,33 +48,87 @@ li a {
   color: white;
   font-family: "Century Gothic";
   text-align: center;
-  padding: 16px 199px;
+  padding: 16px 99.8px;
   text-decoration: none;
 }
 
 li a:hover {
   background-color: #A52121;
+  color: black;
+  opacity: 80%;
 }
 
-.header{
-  /*font-family: "Century Gothic";
-  color: #D02828;
-  background-color: #163C48;*/
-  padding: 0px 0px;
+.fotoPrin {
+  opacity: 98%;
+  height: 250px;
+  margin-top: 35px;
+  border-radius: 50px;
+}
+
+.header {
+  width:30em;
+  height:17em;
+  padding-left: 425px;
+  margin-bottom: 15px;
+}
+
+.f1 {
+  font-family: "Century Gothic";
+  color: white;
+  padding-top: 300px;
+  padding-left: 575px;
+}
+
+#tickets{
+  text-decoration: none;
+  font-family: "Century Gothic";
+  color: white;
+  border-style: solid;
+}
+
+.btnTickets {
+  background-color: #232929;
+  height: 40px;
+  width: 75px;
+  border-radius: 10px;
+  color: white;
+}
+
+.btnTickets:hover {
+  background-color: #A52121;
+  color: black;
 }
 
 </style>
 </head>
 <body>
   <div class="header">
-    <img src="https://i.pinimg.com/474x/d1/29/b6/d129b66cd9fb6a653dac16437ffa0eaf.jpg" alt="">
+    <img class="" src="graphics/RCH.png" alt="">
   </div>
-
-  <ul>
-    <li><a class="active" href="#rc">RC!</a></li>
-    <li><a href="#chivos">Chivos</a></li>
-    <li><a href="#contacto">Contacto</a></li>
+  <ul id="menu">
+    <li><a href="">Soundcloud</a></li>
+    <li><a href="">Twitter</a></li>
+    <li><a href="">Instagram</a></li>
+    <li><a href="">YouTube</a></li>
+    <li><a href="">TikTok</a></li>
   </ul>
+  <div class="fotoPrin">
+    <center><img src="graphics/envivo.jpg" alt=""></center>
+  </div>
+  <br>
+  <div class="f1">
+      <h3>Jazz Café Escazú</h3>
+      <h5>Marzo 3, 2023 - 8pm</h5>
+      <button class="btnTickets" type="button" name="button">Tickets</button>
+
+      <h3>El Observatorio</h3>
+      <h5>Marzo 15, 2023 - 8pm</h5>
+      <button  class="btnTickets" type="button" name="button">Tickets</button>
+
+      <h3>Parque Viva</h3>
+      <h5>Marzo 10, 2023 - 8pm</h5>
+      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  </div>
 </body>
 </html>
 <?php }
