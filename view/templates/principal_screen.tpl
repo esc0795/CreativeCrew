d1c30160a4d85141ffe1d46926979b1eeb5e32d6
esibaja0795 <sibaja0795@gmail.com>
2023-02-14 23:13:42 -0600

    Main Page full TODO: Check Scroll Issue

--- view/templates/principal_screen.tpl
+++ view/templates/principal_screen.tpl
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <style>
-
-body{
+body {
   background-color: black;
+}
+
+.mySlides {
+  display:none;
 }
 
 ul {
@@ -12,7 +17,7 @@ ul {
   margin: 0;
   padding: 0;
   overflow: hidden;
-  background-color: #3D3D3E;
+  background-color: #1F2E1F;
   opacity: 95%;
 }
 
@@ -25,7 +30,7 @@ li a {
   color: white;
   font-family: "Century Gothic";
   text-align: center;
-  padding: 16px 99.8px;
+  padding: 16px 83px;
   text-decoration: none;
 }
 
@@ -36,37 +41,67 @@ li a:hover {
 }
 
 .fotoPrin {
-  opacity: 98%;
+  opacity: 95%;
   height: 250px;
   margin-top: 35px;
   border-radius: 50px;
+  margin-left: 5px;
 }
 
 .header {
   width:30em;
   height:17em;
-  padding-left: 425px;
+  padding-left: 435px;
   margin-bottom: 15px;
 }
 
 .f1 {
   font-family: "Century Gothic";
   color: white;
-  padding-top: 300px;
-  padding-left: 575px;
+  padding-top: 25px;
+  padding-left: 350px;
+  width: 60%;
+  margin-left: 268px;
 }
 
-#tickets{
+td {
+  border-collapse: collapse;
+  border-radius: 5px;
+  opacity: 85%;
+  padding-left: 63px;
+}
+
+th {
+  text-align: justify;
+  padding-left: 79px;
+}
+
+table {
+  border-collapse: collapse;
+  border-radius: 5px;
+  opacity: 85%;
+  margin-left: 150px;
+}
+
+#tickets {
   text-decoration: none;
   font-family: "Century Gothic";
   color: white;
   border-style: solid;
 }
 
+#chivosTxt {
+  color: white;
+  font-family: "Century Gothic";
+  padding-top:20px;
+  padding-left: 605px;
+  opacity: 85%;
+}
+
 .btnTickets {
-  background-color: #232929;
+  background-color: #063E05;
   height: 40px;
-  width: 75px;
+  width: 95px;
   border-radius: 10px;
   color: white;
 }
@@ -76,35 +111,134 @@ li a:hover {
   color: black;
 }
 
+#footer {
+  background-color: #2E5B2D;
+  height: 250px;
+  opacity: 50%;
+  border-style:solid;
+  border-radius: 15px;
+  margin-top: 35px;
+}
+
+.author {
+  text-align: center;
+  font-family: "Century Gothic";
+  font-size: 12px;
+  margin-top: 25px;
+  color: #EEEEEE;
+}
+
+.frase {
+  text-align: center;
+  font-family: "Century Gothic";
+  font-size: 20px;
+  margin-top: 20px;
+  color: #EEEEEE;
+  background-color: #11230F;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
 </style>
 </head>
 <body>
   <div class="header">
     <img class="" src="graphics/RCH.png" alt="">
   </div>
+  <p>&nbsp;</p>
   <ul id="menu">
-    <li><a href="">Soundcloud</a></li>
+    <li><a href="">Música</a></li>
     <li><a href="">Twitter</a></li>
     <li><a href="">Instagram</a></li>
     <li><a href="">YouTube</a></li>
     <li><a href="">TikTok</a></li>
+    <li><a href="">Contacto</a></li>
   </ul>
-  <div class="fotoPrin">
-    <center><img src="graphics/envivo.jpg" alt=""></center>
-  </div>
-  <br>
-  <div class="f1">
-      <h3>Jazz Café Escazú</h3>
-      <h5>Marzo 3, 2023 - 8pm</h5>
-      <button class="btnTickets" type="button" name="button">Tickets</button>
+  <p>&nbsp;</p>
+  <h1 id="chivosTxt">Eventos</h1>
+  <p>&nbsp;</p>
+  <table class="f1">
+  <tr >
+    <th class="">Lugar</th>
+    <th class="">Fecha/Hora</th>
+    <th class="">Entradas</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Jazz Café Escazú</td>
+    <td>Marzo 3, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>El Observatorio</td>
+    <td>Marzo 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Parque Viva</td>
+    <td>Marzo 2, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Ferro Chill</td>
+    <td>Abril 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<div class="w3-content w3-display-container" style="max-width:70%; margin-left:340px;">
+  <img class="mySlides" src="graphics/envivo/caloncho.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/envivo.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/ferreira.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/vega.jpg" style="width:70%; height: 40em">
 
-      <h3>El Observatorio</h3>
-      <h5>Marzo 15, 2023 - 8pm</h5>
-      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)" style="max-width:70%; margin-left:10px;">&#10094;</button>
+  <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)" style="max-width:70%; margin-right: 305px;">&#10095;</button>
+</div>
+<script>
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-      <h3>Parque Viva</h3>
-      <h5>Marzo 10, 2023 - 8pm</h5>
-      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) {
+      slideIndex = 1
+    }
+
+    if (n < 1) {
+      slideIndex = x.length
+    }
+
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+
+    x[slideIndex-1].style.display = "block";
+}
+</script>
+<footer id="footer">
+  <div class="">
+    <p>&nbsp;</p>
   </div>
+  <div class="frase">
+    <center><p>Qué criaturas tan decentes los Ratones Coloraos....</p></center>
+  </div>
+  <div class="author">
+    <center><p>Enoc Sibaja Carrion - Software Developer©</p></center>
+    <center><p><a href="mailto:hege@example.com">info@escarrion.com</a></p></center>
+    <center><p>February, 2023</p></center>
+  </div>
+</footer>
 </body>
 </html>
--- view/templates_c/201e082e24da46c3648012625ccb9f2cda62d8b5_0.file.principal_screen.tpl.php
+++ view/templates_c/201e082e24da46c3648012625ccb9f2cda62d8b5_0.file.principal_screen.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-02-14 22:36:53
+/* Smarty version 4.3.0, created on 2023-02-15 06:12:26
   from 'C:\xampp\htdocs\RatonesColoraos\view\templates\principal_screen.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_63ebfef5052e87_87886706',
+  'unifunc' => 'content_63ec69bab2f337_16993626',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '201e082e24da46c3648012625ccb9f2cda62d8b5' => 
     array (
       0 => 'C:\\xampp\\htdocs\\RatonesColoraos\\view\\templates\\principal_screen.tpl',
-      1 => 1676410611,
+      1 => 1676437945,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,19 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63ebfef5052e87_87886706 (Smarty_Internal_Template $_smarty_tpl) {
+function content_63ec69bab2f337_16993626 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <style>
-
-body{
+body {
   background-color: black;
+}
+
+.mySlides {
+  display:none;
 }
 
 ul {
@@ -35,7 +40,7 @@ ul {
   margin: 0;
   padding: 0;
   overflow: hidden;
-  background-color: #3D3D3E;
+  background-color: #1F2E1F;
   opacity: 95%;
 }
 
@@ -48,7 +53,7 @@ li a {
   color: white;
   font-family: "Century Gothic";
   text-align: center;
-  padding: 16px 99.8px;
+  padding: 16px 83px;
   text-decoration: none;
 }
 
@@ -59,37 +64,67 @@ li a:hover {
 }
 
 .fotoPrin {
-  opacity: 98%;
+  opacity: 95%;
   height: 250px;
   margin-top: 35px;
   border-radius: 50px;
+  margin-left: 5px;
 }
 
 .header {
   width:30em;
   height:17em;
-  padding-left: 425px;
+  padding-left: 435px;
   margin-bottom: 15px;
 }
 
 .f1 {
   font-family: "Century Gothic";
   color: white;
-  padding-top: 300px;
-  padding-left: 575px;
+  padding-top: 25px;
+  padding-left: 350px;
+  width: 60%;
+  margin-left: 268px;
 }
 
-#tickets{
+td {
+  border-collapse: collapse;
+  border-radius: 5px;
+  opacity: 85%;
+  padding-left: 63px;
+}
+
+th {
+  text-align: justify;
+  padding-left: 79px;
+}
+
+table {
+  border-collapse: collapse;
+  border-radius: 5px;
+  opacity: 85%;
+  margin-left: 150px;
+}
+
+#tickets {
   text-decoration: none;
   font-family: "Century Gothic";
   color: white;
   border-style: solid;
 }
 
+#chivosTxt {
+  color: white;
+  font-family: "Century Gothic";
+  padding-top:20px;
+  padding-left: 605px;
+  opacity: 85%;
+}
+
 .btnTickets {
-  background-color: #232929;
+  background-color: #063E05;
   height: 40px;
-  width: 75px;
+  width: 95px;
   border-radius: 10px;
   color: white;
 }
@@ -99,36 +134,137 @@ li a:hover {
   color: black;
 }
 
+#footer {
+  background-color: #2E5B2D;
+  height: 250px;
+  opacity: 50%;
+  border-style:solid;
+  border-radius: 15px;
+  margin-top: 35px;
+}
+
+.author {
+  text-align: center;
+  font-family: "Century Gothic";
+  font-size: 12px;
+  margin-top: 25px;
+  color: #EEEEEE;
+}
+
+.frase {
+  text-align: center;
+  font-family: "Century Gothic";
+  font-size: 20px;
+  margin-top: 20px;
+  color: #EEEEEE;
+  background-color: #11230F;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
 </style>
 </head>
 <body>
   <div class="header">
     <img class="" src="graphics/RCH.png" alt="">
   </div>
+  <p>&nbsp;</p>
   <ul id="menu">
-    <li><a href="">Soundcloud</a></li>
+    <li><a href="">Música</a></li>
     <li><a href="">Twitter</a></li>
     <li><a href="">Instagram</a></li>
     <li><a href="">YouTube</a></li>
     <li><a href="">TikTok</a></li>
+    <li><a href="">Contacto</a></li>
   </ul>
-  <div class="fotoPrin">
-    <center><img src="graphics/envivo.jpg" alt=""></center>
-  </div>
-  <br>
-  <div class="f1">
-      <h3>Jazz Café Escazú</h3>
-      <h5>Marzo 3, 2023 - 8pm</h5>
-      <button class="btnTickets" type="button" name="button">Tickets</button>
+  <p>&nbsp;</p>
+  <h1 id="chivosTxt">Eventos</h1>
+  <p>&nbsp;</p>
+  <table class="f1">
+  <tr >
+    <th class="">Lugar</th>
+    <th class="">Fecha/Hora</th>
+    <th class="">Entradas</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Jazz Café Escazú</td>
+    <td>Marzo 3, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>El Observatorio</td>
+    <td>Marzo 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Parque Viva</td>
+    <td>Marzo 2, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Ferro Chill</td>
+    <td>Abril 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<div class="w3-content w3-display-container" style="max-width:70%; margin-left:340px;">
+  <img class="mySlides" src="graphics/envivo/caloncho.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/envivo.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/ferreira.jpg" style="width:70%; height: 40em">
+  <img class="mySlides" src="graphics/envivo/vega.jpg" style="width:70%; height: 40em">
 
-      <h3>El Observatorio</h3>
-      <h5>Marzo 15, 2023 - 8pm</h5>
-      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)" style="max-width:70%; margin-left:10px;">&#10094;</button>
+  <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)" style="max-width:70%; margin-right: 305px;">&#10095;</button>
+</div>
+<?php echo '<script'; ?>
+>
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-      <h3>Parque Viva</h3>
-      <h5>Marzo 10, 2023 - 8pm</h5>
-      <button  class="btnTickets" type="button" name="button">Tickets</button>
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) {
+      slideIndex = 1
+    }
+
+    if (n < 1) {
+      slideIndex = x.length
+    }
+
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+
+    x[slideIndex-1].style.display = "block";
+}
+<?php echo '</script'; ?>
+>
+<footer id="footer">
+  <div class="">
+    <p>&nbsp;</p>
   </div>
+  <div class="frase">
+    <center><p>Qué criaturas tan decentes los Ratones Coloraos....</p></center>
+  </div>
+  <div class="author">
+    <center><p>Enoc Sibaja Carrion - Software Developer©</p></center>
+    <center><p><a href="mailto:hege@example.com">info@escarrion.com</a></p></center>
+    <center><p>February, 2023</p></center>
+  </div>
+</footer>
 </body>
 </html>
 <?php }
