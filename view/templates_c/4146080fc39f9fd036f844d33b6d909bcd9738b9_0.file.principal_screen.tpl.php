53499d19669f863fb452fd6910631447bd23f5c0
esibaja0795 <sibaja0795@gmail.com>
2023-02-16 00:29:03 -0600

    Changes template

--- view/templates_c/4146080fc39f9fd036f844d33b6d909bcd9738b9_0.file.principal_screen.tpl.php
+++ view/templates_c/4146080fc39f9fd036f844d33b6d909bcd9738b9_0.file.principal_screen.tpl.php
@@ -1,52 +1,32 @@
 <?php
 <<<<<<< HEAD
-/* Smarty version 4.3.0, created on 2023-02-16 07:24:35
-=======
 /* Smarty version 4.3.0, created on 2023-02-16 06:55:14
->>>>>>> faceaac708b060d3a598818ff0e3c40869c3b859
   from 'C:\xampp\htdocs\CreativeCrew\view\templates\principal_screen.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-<<<<<<< HEAD
-  'unifunc' => 'content_63edcc23759373_04757079',
-=======
   'unifunc' => 'content_63edc542983971_92856430',
->>>>>>> faceaac708b060d3a598818ff0e3c40869c3b859
   'has_nocache_code' => false,
-  'file_dependency' => 
+  'file_dependency' =>
   array (
-    '4146080fc39f9fd036f844d33b6d909bcd9738b9' => 
+    '4146080fc39f9fd036f844d33b6d909bcd9738b9' =>
     array (
       0 => 'C:\\xampp\\htdocs\\CreativeCrew\\view\\templates\\principal_screen.tpl',
-<<<<<<< HEAD
-      1 => 1676528672,
-=======
       1 => 1676526913,
->>>>>>> faceaac708b060d3a598818ff0e3c40869c3b859
       2 => 'file',
     ),
   ),
-  'includes' => 
+  'includes' =>
   array (
   ),
 ),false)) {
-<<<<<<< HEAD
-function content_63edcc23759373_04757079 (Smarty_Internal_Template $_smarty_tpl) {
-?><!DOCTYPE html>
-<html>
-<head>
-  <title>Ratones Coloraos!</title>
-  <link rel="icon" type="image/png" href="graphics\RCHEADERB.png">
-=======
 function content_63edc542983971_92856430 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 <head>
   <title>Creative Crew!</title>
   <link rel="icon" type="image/png" href="graphics/TC.png">
->>>>>>> faceaac708b060d3a598818ff0e3c40869c3b859
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -56,70 +36,6 @@ function content_63edc542983971_92856430 (Smarty_Internal_Template $_smarty_tpl)
 </head>
 <body>
   <ul class="active">
-<<<<<<< HEAD
-    <li><a href="">Música</a></li>
-    <li><a href="">Twitter</a></li>
-    <li><a href="">Instagram</a></li>
-    <li><a href="">YouTube</a></li>
-    <li><a href="">TikTok</a></li>
-    <li><a href="">Contacto</a></li>
-  </ul>
-  <p>&nbsp;</p>
-  <div class="header">
-    <img class="" src="graphics/RCH.png" alt="">
-  </div>
-  <p>&nbsp;</p>
-  <h1 id="chivosTxt">Eventos</h1>
-  <p>&nbsp;</p>
-  <table class="f1">
-  <tr >
-    <th class="">Lugar</th>
-    <th class="">Fecha/Hora</th>
-    <th class="">Entradas</th>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>Jazz Café Escazú - San José, CR</td>
-    <td>Marzo 03, 2023 - 8pm</td>
-    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
-  </tr>
-  <tr>
-    <td>El Observatorio - San José, CR</td>
-    <td>Marzo 15, 2023 - 8pm</td>
-    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
-  </tr>
-  <tr>
-    <td>Parque Viva - Alajuela, CR</td>
-    <td>Marzo 20, 2023 - 8pm</td>
-    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
-  </tr>
-  <tr>
-    <td>Ferro Chill - Alajuela, CR</td>
-    <td>Abril 15, 2023 - 8pm</td>
-    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
-  </tr>
-  </table>
-  <p>&nbsp;</p>
-  <div class="w3-content w3-section" style="max-width:500px">
-    <img class="mySlides" src="graphics/envivo/im1.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im2.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im3.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im4.jpg" style="width:100%">
-  </div>
-<footer id="footer">
-  <p>&nbsp;</p>
-  <hr>
-  <div class="frase">
-    <p>Qué criaturas tan decentes los Ratones Coloraos...😎</p>
-  </div>
-  <hr>
-  <div class="author">
-    <p>Enoc Sibaja Carrion&nbsp;©</p>
-    <p><a href="mailto:info@escarrion.com:">info@escarrion.com</a></p>
-    <p>2023</p>
-=======
     <li><a href="">Tech Crew</a></li>
     <li><a href="">Podcast</a></li>
     <li><a href="">Contact</a></li>
@@ -156,7 +72,6 @@ function content_63edc542983971_92856430 (Smarty_Internal_Template $_smarty_tpl)
     <strong><p>Tech Crew!&nbsp;©</p></strong>
     <strong><a href="mailto:info@techcrew.com:">info@techcrew.com</a></p></strong><p>
     <strong><p>2023</p></strong>
->>>>>>> faceaac708b060d3a598818ff0e3c40869c3b859
   </div>
 </footer>
 <?php echo '<script'; ?>
