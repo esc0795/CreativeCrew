85d649def8901cb334eb90ef0e651529c0357c38
esibaja0795 <sibaja0795@gmail.com>
2023-02-16 00:02:50 -0600

    Base of a tech company

--- view/templates/principal_screen.tpl
+++ view/templates/principal_screen.tpl
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Creative Crew!</title>
-  <link rel="icon" type="image/png" href="graphics\RCHEADERB.png">
+  <link rel="icon" type="image/png" href="graphics/TC.png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -12,35 +12,42 @@
 </head>
 <body>
   <ul class="active">
-    <li><a href="">Creative Crew</a></li>
-    <li><a href="">Twitter</a></li>
-    <li><a href="">Contacto</a></li>
+    <li><a href="">Tech Crew</a></li>
+    <li><a href="">Podcast</a></li>
+    <li><a href="">Contact</a></li>
   </ul>
   <p>&nbsp;</p>
   <div class="header">
-    <img class="" src="graphics/CC.png" alt="">
+    <img class="" src="graphics/TC.png" alt="">
   </div>
   <p>&nbsp;</p>
-  <h1 id="chivosTxt">Eventos</h1>
   <p>&nbsp;</p>
-  <p>&nbsp;</p>
+    <strong><p id="frase1">HAVING FUN EVERYDAY!</p></strong>
+    <img class="gif1" src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif" alt="">
+    <p>&nbsp;</p>
+    <p id="frase">Web Development & Tech Podcast Things</p>
+    <p>&nbsp;</p>
   <div class="w3-content w3-section" style="max-width:500px">
-    <img class="mySlides" src="graphics/envivo/im1.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im2.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im3.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im4.jpg" style="width:100%">
+    <img class="mySlides" src="graphics/envivo/img0.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp1.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/ima2.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp2.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img3.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp3.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img4.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img5.jpg" style="width:150%; height:200%">
   </div>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
 <footer id="footer">
   <p>&nbsp;</p>
   <hr>
-  <div class="frase">
-    <p>Qué criaturas tan decentes los Ratones Coloraos...😎</p>
-  </div>
   <hr>
   <div class="author">
-    <p>Enoc Sibaja Carrion&nbsp;©</p>
-    <p><a href="mailto:info@escarrion.com:">info@escarrion.com</a></p>
-    <p>2023</p>
+    <strong><p>Tech Crew!&nbsp;©</p></strong>
+    <strong><a href="mailto:info@techcrew.com:">info@techcrew.com</a></p></strong><p>
+    <strong><p>2023</p></strong>
   </div>
 </footer>
 <script src="js/jsMainPage.js"></script>
--- view/templates_c/4146080fc39f9fd036f844d33b6d909bcd9738b9_0.file.principal_screen.tpl.php
+++ view/templates_c/4146080fc39f9fd036f844d33b6d909bcd9738b9_0.file.principal_screen.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-02-16 00:09:28
+/* Smarty version 4.3.0, created on 2023-02-16 06:55:14
   from 'C:\xampp\htdocs\CreativeCrew\view\templates\principal_screen.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_63ed662846dba9_33972761',
+  'unifunc' => 'content_63edc542983971_92856430',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '4146080fc39f9fd036f844d33b6d909bcd9738b9' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CreativeCrew\\view\\templates\\principal_screen.tpl',
-      1 => 1676502566,
+      1 => 1676526913,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,12 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63ed662846dba9_33972761 (Smarty_Internal_Template $_smarty_tpl) {
+function content_63edc542983971_92856430 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 <head>
   <title>Creative Crew!</title>
-  <link rel="icon" type="image/png" href="graphics\RCHEADERB.png">
+  <link rel="icon" type="image/png" href="graphics/TC.png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -35,35 +35,42 @@ function content_63ed662846dba9_33972761 (Smarty_Internal_Template $_smarty_tpl)
 </head>
 <body>
   <ul class="active">
-    <li><a href="">Creative Crew</a></li>
-    <li><a href="">Twitter</a></li>
-    <li><a href="">Contacto</a></li>
+    <li><a href="">Tech Crew</a></li>
+    <li><a href="">Podcast</a></li>
+    <li><a href="">Contact</a></li>
   </ul>
   <p>&nbsp;</p>
   <div class="header">
-    <img class="" src="graphics/CC.png" alt="">
+    <img class="" src="graphics/TC.png" alt="">
   </div>
   <p>&nbsp;</p>
-  <h1 id="chivosTxt">Eventos</h1>
   <p>&nbsp;</p>
-  <p>&nbsp;</p>
+    <strong><p id="frase1">HAVING FUN EVERYDAY!</p></strong>
+    <img class="gif1" src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif" alt="">
+    <p>&nbsp;</p>
+    <p id="frase">Web Development & Tech Podcast Things</p>
+    <p>&nbsp;</p>
   <div class="w3-content w3-section" style="max-width:500px">
-    <img class="mySlides" src="graphics/envivo/im1.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im2.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im3.jpg" style="width:100%">
-    <img class="mySlides" src="graphics/envivo/im4.jpg" style="width:100%">
+    <img class="mySlides" src="graphics/envivo/img0.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp1.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/ima2.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp2.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img3.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/imp3.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img4.jpg" style="width:150%; height:200%">
+    <img class="mySlides" src="graphics/envivo/img5.jpg" style="width:150%; height:200%">
   </div>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
 <footer id="footer">
   <p>&nbsp;</p>
   <hr>
-  <div class="frase">
-    <p>Qué criaturas tan decentes los Ratones Coloraos...😎</p>
-  </div>
   <hr>
   <div class="author">
-    <p>Enoc Sibaja Carrion&nbsp;©</p>
-    <p><a href="mailto:info@escarrion.com:">info@escarrion.com</a></p>
-    <p>2023</p>
+    <strong><p>Tech Crew!&nbsp;©</p></strong>
+    <strong><a href="mailto:info@techcrew.com:">info@techcrew.com</a></p></strong><p>
+    <strong><p>2023</p></strong>
   </div>
 </footer>
 <?php echo '<script'; ?>
