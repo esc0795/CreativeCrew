f910c8267a220d0a5768f153bd4cb5555db217ca
esibaja0795 <sibaja0795@gmail.com>
2023-02-16 00:25:34 -0600

    Update Page with thech project

---
+++ view/templates_c/4146080fc39f9fd036f844d33b6d909bcd9738b9_0.file.principal_screen.tpl.php
@@ -0,0 +1,108 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-02-16 07:24:35
+  from 'C:\xampp\htdocs\CreativeCrew\view\templates\principal_screen.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_63edcc23759373_04757079',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4146080fc39f9fd036f844d33b6d909bcd9738b9' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\CreativeCrew\\view\\templates\\principal_screen.tpl',
+      1 => 1676528672,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63edcc23759373_04757079 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html>
+<head>
+  <title>Ratones Coloraos!</title>
+  <link rel="icon" type="image/png" href="graphics\RCHEADERB.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style type="text/css">
+  @import url("css/styleMainPage.css");
+</style>
+</head>
+<body>
+  <ul class="active">
+    <li><a href="">Música</a></li>
+    <li><a href="">Twitter</a></li>
+    <li><a href="">Instagram</a></li>
+    <li><a href="">YouTube</a></li>
+    <li><a href="">TikTok</a></li>
+    <li><a href="">Contacto</a></li>
+  </ul>
+  <p>&nbsp;</p>
+  <div class="header">
+    <img class="" src="graphics/RCH.png" alt="">
+  </div>
+  <p>&nbsp;</p>
+  <h1 id="chivosTxt">Eventos</h1>
+  <p>&nbsp;</p>
+  <table class="f1">
+  <tr >
+    <th class="">Lugar</th>
+    <th class="">Fecha/Hora</th>
+    <th class="">Entradas</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Jazz Café Escazú - San José, CR</td>
+    <td>Marzo 03, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>El Observatorio - San José, CR</td>
+    <td>Marzo 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Parque Viva - Alajuela, CR</td>
+    <td>Marzo 20, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  <tr>
+    <td>Ferro Chill - Alajuela, CR</td>
+    <td>Abril 15, 2023 - 8pm</td>
+    <td><button class="btnTickets" type="button" name="button">Comprar</button></td>
+  </tr>
+  </table>
+  <p>&nbsp;</p>
+  <div class="w3-content w3-section" style="max-width:500px">
+    <img class="mySlides" src="graphics/envivo/im1.jpg" style="width:100%">
+    <img class="mySlides" src="graphics/envivo/im2.jpg" style="width:100%">
+    <img class="mySlides" src="graphics/envivo/im3.jpg" style="width:100%">
+    <img class="mySlides" src="graphics/envivo/im4.jpg" style="width:100%">
+  </div>
+<footer id="footer">
+  <p>&nbsp;</p>
+  <hr>
+  <div class="frase">
+    <p>Qué criaturas tan decentes los Ratones Coloraos...😎</p>
+  </div>
+  <hr>
+  <div class="author">
+    <p>Enoc Sibaja Carrion&nbsp;©</p>
+    <p><a href="mailto:info@escarrion.com:">info@escarrion.com</a></p>
+    <p>2023</p>
+  </div>
+</footer>
+<?php echo '<script'; ?>
+ src="js/jsMainPage.js"><?php echo '</script'; ?>
+>
+</body>
+</html>
+<?php }
+}
