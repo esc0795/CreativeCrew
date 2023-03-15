63d78af7208dc71ae1754972c1410db9ea6c49d8
esibaja0795 <sibaja0795@gmail.com>
2023-03-15 02:35:54 -0600

    Set Soundcloud API connection

--- podcast.php
+++ podcast.php
@@ -7,4 +7,11 @@ $objControl->displayHeader();
 $objControl->displayPodcast();
 $objControl->displayFooter();
 
+//Consumir API de Soundcloud
+$track_id = 'hyshopd154a';
+$client_id = 'enoc-sibaja';
+$url = 'https://api.soundcloud.com/tracks/'.$track_id.'.json?client_id='.$client_id;
+
+$track_json = file_get_contents($url);
+$track = json_decode($track_json);
 ?>
