855d01a913de94d972f61d8198c72cd2c193de79
esibaja0795 <sibaja0795@gmail.com>
2023-03-15 12:33:14 -0600

    Avoid error from soundcloud reques

--- podcast.php
+++ podcast.php
@@ -11,7 +11,4 @@ $objControl->displayFooter();
 $track_id = 'hyshopd154aDFD';
 $client_id = 'enoc-sibaja';
 $url = 'https://api.soundcloud.com/tracks/'.$track_id.'.json?client_id='.$client_id;
-
-$track_json = file_get_contents($url);
-$track = json_decode($track_json);
 ?>
